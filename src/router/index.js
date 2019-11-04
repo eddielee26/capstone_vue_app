0e9c5e1fb34469a7b413ab032e1325b0ab7bca84
eddie <eddielee001@gmail.com>
2019-11-04 12:39:27 -0600

    moves login, logout, signup to users folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
-import Signup from "../views/Signup.vue";
-import Login from "../views/Login.vue";
-import Logout from "../views/Logout.vue";
+import Signup from "../views/users/Signup.vue";
+import Login from "../views/users/Login.vue";
+import Logout from "../views/users/Logout.vue";
 
 Vue.use(VueRouter);
 
