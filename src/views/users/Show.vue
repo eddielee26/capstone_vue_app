77c972a786c02339f79b3cc8e0112235d94aff2e
eddie <eddielee001@gmail.com>
2019-11-11 15:36:46 -0600

    edits user graph

--- src/views/users/Show.vue
+++ src/views/users/Show.vue
@@ -11,6 +11,16 @@
     <router-link v-bind:to="`/users/${user_id}/edit`">Edit</router-link>
 
     <div id="container" style="width:100%; height:400px;"></div>
+    
+    <input type="number" v-model="benchWeight">
+
+    <form v-on:submit.prevent="submit()">
+      <div class="form-group">
+        <label>Bench weight</label> 
+        <input type="number" class="form-control" v-model="benchWeight">
+      </div>
+      <input type="submit" class="btn btn-primary" value="Add Bench Weight"><br><br>
+    </form>
 
   </div>
 
@@ -58,77 +68,14 @@
 import axios from "axios";
 import Highcharts from 'highcharts';
 
-Highcharts.chart('container', {
-  chart: {
-    type: 'column'
-  },
-  title: {
-    text: 'Monthly Average Rainfall'
-  },
-  subtitle: {
-    text: 'Source: WorldClimate.com'
-  },
-  xAxis: {
-    categories: [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec'
-    ],
-    crosshair: true
-  },
-  yAxis: {
-    min: 0,
-    title: {
-      text: 'Rainfall (mm)'
-    }
-  },
-  tooltip: {
-    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-    pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-    footerFormat: '</table>',
-    shared: true,
-    useHTML: true
-  },
-  plotOptions: {
-    column: {
-      pointPadding: 0.2,
-      borderWidth: 0
-    }
-  },
-  series: [{
-    name: 'Tokyo',
-    data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
-
-  }, {
-    name: 'New York',
-    data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-
-  }, {
-    name: 'London',
-    data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-  }, {
-    name: 'Berlin',
-    data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
-
-  }]
-});
-
 export default {
   data: function() {
     return {
       user: {},
       user_id: localStorage.getItem("user_id"),
+      benchWeight: "",
+      squatWeight: "",
+      deadliftWeight: "",
     };
   },
   created: function() {
@@ -137,6 +84,78 @@ export default {
       console.log(this.user);
     });
   },
-  methods: {}
+  mounted: function() {
+    Highcharts.chart('container', {
+      chart: {
+        type: 'column'
+      },
+      title: {
+        text: 'ONE REP MAX (ORM)'
+      },
+      subtitle: {
+        text: ''
+      },
+      xAxis: {
+        categories: [
+          'Bench',
+          'Bench Estimate',
+          'Squat',
+          'Squat Estimate',
+          'Deadlift',
+          'Deadlift Estimate',
+        ],
+        crosshair: true
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Weight (lbs)'
+        }
+      },
+      tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style="padding:0"><b>{point.y:.1f} lbs</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+      },
+      plotOptions: {
+        column: {
+          pointPadding: 0.2,
+          borderWidth: 0
+        }
+      },
+      series: 
+      [
+        {
+          name: 'Bench',
+          data: [this.benchWeight]
+
+        }, {
+          name: 'Bench Estimate',
+          data: [170]
+
+        }, {
+          name: 'Squat',
+          data: [240]
+
+        }, {
+          name: 'Squat Estimate',
+          data: [265]
+
+        }, {
+          name: 'Deadlift',
+          data: [295]
+
+        }, {
+          name: 'Deadlift Estimate',
+          data: [315]
+
+        }],
+    });
+  },
+  methods: {
+  },
 };
 </script>
