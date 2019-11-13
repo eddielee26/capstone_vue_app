7e0667adda7281183d994b94d4151d695a159bf6
eddie <eddielee001@gmail.com>
2019-11-13 12:17:52 -0600

    adds user login theme

--- src/views/users/Login.vue
+++ src/views/users/Login.vue
@@ -1,6 +1,53 @@
 <template>
   <div class="login">
-    <div class="container">
+
+    <!-- ===========================
+    =====>> Page Hero <<===== -->
+    <section id="page-hero" class="about-bg">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="page-title text-center">
+              <h1>USER <span>LOGIN</span></h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- =====>> End Page Hero <<===== 
+    =========================== -->
+
+    <!-- ===========================
+    =====>> My Account <<===== -->
+    <section id="my-account-area" class="pt-50 pb-50">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <div class="my-account-content">
+                        <h2>Login</h2><br><br>
+                        <form v-on:submit.prevent="submit()">
+                          <ul>
+                            <li class="text-danger" v-for="error in errors">{{ error }}</li>
+                          </ul>
+                          <div class="form-group">
+                            <label>Email:</label>
+                            <input type="email" class="form-control" v-model="email">
+                          </div>
+                          <div class="form-group">
+                            <label>Password:</label>
+                            <input type="password" class="form-control" v-model="password">
+                          </div>
+                          <input type="submit" class="btn btn-primary" value="Login">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- =====>> End My Account <<===== 
+    =========================== -->
+
+<!--     <div class="container">
       <form v-on:submit.prevent="submit()">
         <h1>Login</h1>
         <ul>
@@ -16,7 +63,7 @@
         </div>
         <input type="submit" class="btn btn-primary" value="Login">
       </form>
-    </div>
+    </div> -->
   </div>
 </template>
 
